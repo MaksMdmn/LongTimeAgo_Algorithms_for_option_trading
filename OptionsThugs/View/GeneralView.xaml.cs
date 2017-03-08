7618f89176b166fccc63f059674397a4c4103d13
MaksMdmn <maks.mdmn@icloud.com>
2017-03-08 09:49:21 +0200

    Add project

---
+++ OptionsThugs/View/GeneralView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OptionsThugs.View
+{
+    /// <summary>
+    /// Interaction logic for GeneralView.xaml
+    /// </summary>
+    public partial class GeneralView : Window
+    {
+        public GeneralView()
+        {
+            InitializeComponent();
+        }
+    }
+}
